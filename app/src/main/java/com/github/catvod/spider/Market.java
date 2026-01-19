c61f8eae9418f278c20fb5ce1fe3a412f476c1be
dlgt7 <102397160+dlgt7@users.noreply.github.com>
2026-01-19 14:29:33 +0800

    Add files via upload

--- app/src/main/java/com/github/catvod/spider/Market.java
+++ app/src/main/java/com/github/catvod/spider/Market.java
@@ -52,6 +52,7 @@ public class Market extends Spider {
     @Override
     public String action(String action) {
         try {
+            OkHttp.cancel(TAG);
             String name = Uri.parse(action).getLastPathSegment();
             Notify.show("正在下載..." + name);
             Response response = OkHttp.newCall(action, TAG);
@@ -89,6 +90,6 @@ public class Market extends Spider {
 
     @Override
     public void destroy() {
-        // OkHttp.cancel(TAG); // 当前版本OkHttp不支持按标签取消请求
+        OkHttp.cancel(TAG);
     }
 }
