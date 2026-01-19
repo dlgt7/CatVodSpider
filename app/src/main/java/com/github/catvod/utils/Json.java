6a3894d9768b2b6f876fd4acea0ca77caca899e6
dlgt7 <102397160+dlgt7@users.noreply.github.com>
2026-01-19 13:58:25 +0800

    Add files via upload

--- app/src/main/java/com/github/catvod/utils/Json.java
+++ app/src/main/java/com/github/catvod/utils/Json.java
@@ -1,10 +1,13 @@
 package com.github.catvod.utils;
 
+import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 public class Json {
+
+    private static final Gson gson = new Gson();
 
     public static JsonElement parse(String json) {
         try {
@@ -20,6 +23,28 @@ public class Json {
             return obj == null ? new JsonObject() : obj;
         } catch (Throwable e) {
             return new JsonObject();
+        }
+    }
+
+    // 提供向后兼容的Gson访问方法
+    public static Gson getGson() {
+        return gson;
+    }
+
+    // 便捷的序列化和反序列化方法
+    public static <T> T toObject(String json, Class<T> classOfT) {
+        try {
+            return getGson().fromJson(json, classOfT);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public static String toJson(Object src) {
+        try {
+            return getGson().toJson(src);
+        } catch (Exception e) {
+            return "";
         }
     }
 }
