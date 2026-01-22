0b08a140e948bd2d60493e9b34f69852b9947a24
dlgt6 <157378128+dlgt6@users.noreply.github.com>
2026-01-23 04:16:37 +0800

    Update Sub.java

--- app/src/main/java/com/github/catvod/bean/Sub.java
+++ app/src/main/java/com/github/catvod/bean/Sub.java
@@ -1,19 +1,37 @@
 package com.github.catvod.bean;
 
 import com.google.gson.annotations.SerializedName;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Sub {
 
-    @SerializedName("url")
-    private String url;
     @SerializedName("name")
     private String name;
+
+    @SerializedName("url")
+    private String url;
+
+    @SerializedName("ext")
+    private String ext;
+
     @SerializedName("lang")
     private String lang;
+
     @SerializedName("format")
     private String format;
+
     @SerializedName("flag")
-    private int flag;
+    private Integer flag; // 2 代表 forced (强制字幕)
+
+    private static final Map<String, String> MIME_TYPES = new HashMap<>();
+
+    static {
+        MIME_TYPES.put("vtt", "text/vtt");
+        MIME_TYPES.put("ass", "text/x-ssa");
+        MIME_TYPES.put("ssa", "text/x-ssa");
+        MIME_TYPES.put("srt", "application/x-subrip");
+    }
 
     public static Sub create() {
         return new Sub();
@@ -29,13 +47,15 @@ public class Sub {
         return this;
     }
 
-    public Sub lang(String lang) {
-        this.lang = lang;
+    public Sub ext(String ext) {
+        this.ext = ext.toLowerCase();
+        // 自动根据后缀名设置播放器识别的 MIME Type
+        this.format = MIME_TYPES.getOrDefault(this.ext, "application/x-subrip");
         return this;
     }
 
-    private Sub format(String format) {
-        this.format = format;
+    public Sub lang(String lang) {
+        this.lang = lang;
         return this;
     }
 
@@ -44,15 +64,11 @@ public class Sub {
         return this;
     }
 
-    public Sub ext(String ext) {
-        switch (ext) {
-            case "vtt":
-                return format("text/vtt");
-            case "ass":
-            case "ssa":
-                return format("text/x-ssa");
-            default:
-                return format("application/x-subrip");
-        }
-    }
+    // --- Getter 方法，方便播放器调用 ---
+    public String getName() { return name; }
+    public String getUrl() { return url; }
+    public String getExt() { return ext; }
+    public String getLang() { return lang; }
+    public String getFormat() { return format; }
+    public Integer getFlag() { return flag; }
 }
