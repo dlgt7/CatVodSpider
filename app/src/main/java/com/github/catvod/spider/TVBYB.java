f3bf6c9966dbebcf5f697e1724cc22b18d174379
dlgt7 <102397160+dlgt7@users.noreply.github.com>
2026-06-22 20:16:38 +0800

    Add files via upload

--- app/src/main/java/com/github/catvod/spider/TVBYB.java
+++ app/src/main/java/com/github/catvod/spider/TVBYB.java
@@ -31,7 +31,7 @@ public class TVBYB extends Spider {
 
     private final HashMap<String, String> headers = new HashMap<String, String>() {{
         put("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36");
-    };
+    }};
 
     @Override
     public String getName() {
