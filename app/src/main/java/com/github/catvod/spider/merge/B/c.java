50eff0f136f8fc7f569da730114f9aa9c1f8088b
dlgt7 <102397160+dlgt7@users.noreply.github.com>
2026-06-24 12:52:33 +0800

    Add files via upload

--- app/src/main/java/com/github/catvod/spider/merge/B/c.java
+++ app/src/main/java/com/github/catvod/spider/merge/B/c.java
@@ -1,1 +1,16 @@
-1
+package com.github.catvod.spider.merge.B;
+
+import java.nio.charset.StandardCharsets;
+
+/**
+ * Charset 工具类
+ */
+public final class c {
+
+    /**
+     * 返回 UTF-8 Charset
+     */
+    public static StandardCharsets c() {
+        return StandardCharsets.UTF_8;
+    }
+}
